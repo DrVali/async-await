3d88bc9ef7ace0bbc04a77a15c65a4ea84f2d7f0
Ali Ahmadi <ahmadi.world@gmail.com>
2024-11-20 17:21:14 +0330

    Update mainjs.js

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,15 +1,41 @@
-let contery = "iran";
-const getcontery = async function name(countryName) {
+
+const getCountery = async function name(countryName) {
   try {
     const response = await fetch(
       `https://restcountries.com/v3.1/name/${countryName}`
     );
     const data = await response.json();
+    console.log(data[0]);
 
-    console.log(data.name);
+    const mainDiv = document.querySelector(".main");
+    const H2El = document.createElement("h2");
+    H2El.classList.add("counter-title");
+    H2El.innerHTML = `name is : ${data[0].name.common} </br>`;
+    mainDiv.append(H2El);
+
+    const spanPop = document.createElement("span");
+    spanPop.classList.add("pop");
+    spanPop.classList.add("spanId");
+    spanPop.innerHTML = `population:  ${data[0].population} </br>`;
+    mainDiv.append(spanPop);
+
+    const spanRegion = document.createElement("span");
+    spanRegion.classList.add("region");
+    spanRegion.classList.add("spanId");
+    spanRegion.innerHTML = `region:  ${data[0].region} </br>`;
+    mainDiv.append(spanRegion);
+
+    const spancapital = document.createElement("span");
+    spancapital.classList.add("capital");
+    spancapital.innerHTML = `capital: ${data[0].capital} </br>`;
+    mainDiv.append(spancapital);
   } catch (err) {
-    console.log(`error message is :====>   ${err.message}`);
+    console.log(`error message is :====>   ${err.message} </br>`);
   }
 };
-
-getcontery(contery);
+let germanyC = "germany";
+const germany = document.querySelector(".germany");
+germany.addEventListener("click", (e) => {
+  let RegionEl = e.target.value;
+  getCountery(RegionEl);
+});
